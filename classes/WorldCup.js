1baaa83c5d797ab185895d2acee8f49527ef74d9
Manuel Hernandez <cmanuelhm87@gmail.com>
2021-01-13 11:42:48 +0100

    Creando Clases para liguillas

---
+++ classes/WorldCup.js
@@ -0,0 +1,10 @@
+export default class WorldCup {
+
+    constructor(name, teams=[], rounds=1) {
+        this.name = name
+        this.teams = teams
+        this.rounds = rounds
+        this.matchDaySchedule = []
+    }
+
+}
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import { worldCupContenders , 
     grupoA, grupoB, grupoC, grupoD, grupoE, grupoF, grupoG, grupoH} from './teams.js';
+import WorldCup from './classes/WorldCup.js';
+import LeagueWC from './classes/LeagueStage.js';
 
 
+const groups = new LeagueWC ('Group A', grupoA);
 
-console.log(worldCupContenders);
-console.log('GrupoA');
-console.log(grupoA);
+for (const team of groups.teams) {
+    console.log(team);
+}
